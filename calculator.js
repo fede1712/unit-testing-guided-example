9d5c38f92b28a3d84fd0291cb30197396a37f0f4
Fede <fedemarinasruiz@gmail.com>
2021-08-12 19:17:13 +0200

    done

--- calculator.js
+++ calculator.js
@@ -1,13 +1,37 @@
 function sum(a, b) {
-  return;
+  let num1 = a
+  let num2 = b
+  if (num1 === undefined && num2 === undefined) {
+    return 0;
+  } else if (num1 === undefined) {
+    return b;
+  } else if (num2 === undefined) {
+    return a;
+  }
+  return a + b;
 }
 
 function subtract(a, b) {
-  return;
+  let num1 = a;
+  let num2 = b;
+  if (num1 === undefined && num2 === undefined) {
+    return 0;
+  } else if (num1 === undefined) {
+    return num2
+  } else if (num2 === undefined) {
+    return num1
+  }
+  return a - b;
 }
 
 function divide(a, b) {
-  return;
+  let num1 = a;
+  let num2 = b;
+  if (num1 === 0 || num2 === 0) {
+    throw new Error
+  }
+  return a / b;
+
 }
 
 function multiply(a, b) {
